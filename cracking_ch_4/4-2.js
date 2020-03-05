bac711531345c92f2ec3cee4b7d09a6bbbd6c3f3
Ryan <ryan@dks-macbook-pro.home>
2020-03-04 20:49:53 -0800

    Solves algorithm creating BST given sorted array.

---
+++ cracking_ch_4/4-2.js
@@ -0,0 +1,26 @@
+let findMinimalTree = (arr) => {
+  let mid = Math.floor(arr.length / 2);
+  let root = new Node(arr[mid]);
+  if (arr.length <= 1) {
+    return new Node(arr[0]);
+  }
+
+  root.left = findMinimalTree(arr.slice(0, mid));
+  root.right = findMinimalTree(arr.slice(mid + 1, arr.length));
+  return root;
+}
+
+// let slicer = (arr) => {
+//   let mid = arr[Math.floor(arr.length / 2)];
+
+// }
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+console.log(findMinimalTree([1,2,3,4,5,6,7]));
